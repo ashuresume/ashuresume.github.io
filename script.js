da45978ad019297f005e6f527155210162747d04
ashu <uhsa30@gmail.com>
2017-07-05 16:48:56 +0530

    sec website

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	$("#info").click(function(){
+		$("#info").slideUp().slideDown();
+		});
+	
+	$("#s0").mouseenter(function(){
+		$("#s0").animate({left: '450px'});
+	});
+	
+	$("#s1").mouseenter(function(){
+		$("#s1").animate({left: '250px'});
+	});
+
+	$("#s2").mouseenter(function(){
+		$("#s2").animate({left: '650px'});
+	});
+	
+	$("#s3").mouseenter(function(){
+		$("#s3").animate({left: '250px'});
+	});
+	
+	$("#s4").mouseenter(function(){
+		$("#s4").animate({left: '650px'});
+	});
+	
+	$("#s5").mouseenter(function(){
+		$("#s5").animate({left: '250px'});
+	});
+	
+	$("#s6").mouseenter(function(){
+		$("#s6").animate({left: '650px'});
+	});
+});
